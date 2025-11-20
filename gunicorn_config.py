17df194580675a59fada0f2dd2b8e03c350c7fd8
Amanda McNesby <mcnesbya@moravian.edu>
2025-11-20 15:10:23 -0500

    removing files

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-from config import initialize_database
-
-# This Gunicorn hook runs AFTER the application module is loaded 
-# but BEFORE the worker starts handling requests.
-def post_worker_init(worker):
-    """Initializes the database within the Gunicorn worker process."""
-    initialize_database()
-    worker.log.info("Database initialized successfully inside worker.")
